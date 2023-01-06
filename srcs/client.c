3d244344a38c2eed912fd59a67a23acc87f0844f
Tom GERNEZ <tgernez@student.42.fr>
2023-01-06 19:01:45 +0100

    SEMIAUTO-SAFEGUARD

--- srcs/client.c
+++ srcs/client.c
@@ -6,16 +6,81 @@
 /*   By: tgernez <tgernez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 17:47:03 by tgernez           #+#    #+#             */
-/*   Updated: 2023/01/05 17:41:22 by tgernez          ###   ########.fr       */
+/*   Updated: 2023/01/06 18:13:08 by tgernez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void handler(int num)
+static unsigned char *buff_filler(unsigned char c)
 {
-	(void)num;
-	write(1, "J'ai recu le signal ehehe\n", 27);
+	int	i;
+	unsigned char *buff;
+
+	i = 0;
+	buff = malloc(9);
+	if (!buff)
+		return (NULL);
+	buff[8] = '\0';
+	// ft_printf("%c\n", c);
+	while (i < 8)
+	{
+		buff[7 - i] = c & 1;
+		// ft_printf("C & 1 %d /C & 1\n", c & 1);
+		c = c >> 1;
+		i++;
+	}
+
+	// i = 0;
+	// while (i < 8)
+	// {
+	// 	ft_printf("%d", buff[i]);
+	// 	i++;
+	// }
+	// ft_printf("\n");
+	return (buff);
+}
+
+static void send_buff(unsigned char *buff, pid_t pid)
+{
+	int	i;
+	
+	i = 0;
+	while (i < 8)
+	{
+		if (buff[i] == 0)
+			kill(pid, SIGUSR1);
+		else if (buff[i] == 1)
+			kill(pid, SIGUSR2);
+		usleep(50);
+		i++;
+	}
+}
+
+static int	transmitter(char *message, pid_t pid)
+{
+	int	len;
+	int	i;
+	int j;
+	unsigned char *buff;
+
+	i = 0;
+	j = 0;
+	len = 0;
+	while (message[len])
+		len++;
+	ft_printf("LEN %d /LEN\n", len);
+	while (i < len)
+	{
+		buff = buff_filler(message[i]);
+		// ft_printf("BUFF %c /BUFF\n", *buff);
+		if (!buff)
+			return (ft_printf("Buff Alloc Crashed\n"), -1);
+		send_buff(buff, pid);
+		free(buff);
+		i++;
+	}
+	return (1);
 }
 
 pid_t	get_pid(char *str)
@@ -25,7 +90,7 @@ pid_t	get_pid(char *str)
 
 	i = 0;
 	pid = 0;
-	while (str[i])
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
 	{
 		pid = pid * 10 + (str[i] - '0');
 		i++;
@@ -40,8 +105,9 @@ int main(int ac, char **av)
 	if (ac != 3)
 		return (ft_printf("Problem in arguments\n"), -1);
 	server_pid = get_pid(av[1]);
-	if (server_pid < 0)
+	if (server_pid <= 0)
 		return (ft_printf("%d is an incorrect PID\n", server_pid), -1);
-	ft_printf("SERVER PID=%d", server_pid);
+	ft_printf("SERVER PID=%d\n", server_pid);
+	transmitter(av[2], server_pid);
 	return (0);
 }
--- srcs/server.c
+++ srcs/server.c
@@ -6,29 +6,52 @@
 /*   By: tgernez <tgernez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 17:47:10 by tgernez           #+#    #+#             */
-/*   Updated: 2023/01/05 17:53:24 by tgernez          ###   ########.fr       */
+/*   Updated: 2023/01/06 18:30:10 by tgernez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void handler(int num)
-{
-	ft_printf("%d", num);
-}
+static volatile int client_pid = -1;
 
+static void signal_reception(int sig, siginfo_t *info, void *context)
+{
+	static unsigned char buff = 0;
+	static int	factor = 128;
+	
+	(void)context;
+	client_pid = info->si_pid;
+	buff += (sig == SIGUSR2) * factor;
+	factor /= 2;
+	ft_printf("%d\n", factor);
+	// ft_printf("%d", num == SIGUSR2);
+	if (factor == 0)
+	{
+		ft_printf("BUFF %c /BUFF\n", buff);
+		factor = 128;
+		buff = 0;
+		return ;
+	}
+	
+}
 
 int main()
 {
 	struct sigaction	sa;
 	pid_t	pid;
 
+	static int count = 0;
+
 	pid = getpid();
-	sa.sa_handler = &handler;
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = &signal_reception;
+	ft_printf("COUNT %d /COUNT\n", count++);
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
-		return (-1);
-	printf("%d\n", pid);
-	while (1)
+		return (EXIT_FAILURE);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		return (EXIT_FAILURE);
+	ft_printf("SERVER PID= %d\n", pid);
+	while (0b101010)
 		pause();
 	return (0);
 }
